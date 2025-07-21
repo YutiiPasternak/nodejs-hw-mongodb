2eda65a3880004a8a745f1d105947e2f774ad35e
Yurii Pasternak <yuriipasternak@icloud.com>
2025-07-21 22:43:09 +0200

    validation-2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,7 +10,8 @@ import {
   createContactSchema,
   updateContactSchema,
 } from '../validationSchemas/contactsSchemas.js';
-
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
