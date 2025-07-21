4166b0021c8d37eeb98ae1b0d816e228b210f272
Yurii Pasternak <yuriipasternak@icloud.com>
2025-07-21 22:38:38 +0200

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,15 +6,32 @@ import {
   updateContactController,
   deleteContactController,
 } from '../controllers/contacts.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validationSchemas/contactsSchemas.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
 router.get('/contacts', ctrlWrapper(getContacts));
-router.get('/contacts/:contactId', ctrlWrapper(getContact));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContact));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactController),
+);
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default router;
