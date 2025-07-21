5d132cbf4bd0677e11a07d94f9ef581e10e16bd8
Yurii Pasternak <yuriipasternak@icloud.com>
2025-07-21 22:38:38 +0200

    validation

---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,12 @@
+export const validateBody = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+      const message = error.details.map((detail) => detail.message).join(', ');
+      return res.status(400).json({ status: 400, message });
+    }
+
+    next();
+  };
+};
