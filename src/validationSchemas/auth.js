39f020b1466d4af013d467f251a26037be937060
Yurii Pasternak <yuriipasternak@icloud.com>
2025-08-13 19:36:43 +0200

    error-valid

--- src/validationSchemas/auth.js
+++ src/validationSchemas/auth.js
@@ -11,7 +11,7 @@ export const loginUserSchema = Joi.object({
   password: Joi.string().required(),
 });
 
-export const sendRestEmailSchema = Joi.object({
+export const sendResetEmailSchema = Joi.object({
   email: Joi.string().email().required(),
 });
 
