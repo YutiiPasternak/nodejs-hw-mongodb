6ec0026171704a1ab4e6b8445e474b521da9a5b7
Yurii Pasternak <yuriipasternak@icloud.com>
2025-08-14 14:58:15 +0200

    paginacja

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,16 +1,14 @@
-export const calculatePaginationData = ({
-  totalItems,
-  currentPage,
-  perPage,
-}) => {
-  const totalPages = Math.ceil(totalItems / perPage);
+export const calculatePaginationData = (count, perPage, page) => {
+  const totalPages = Math.ceil(count / perPage);
+  const hasNextPage = Boolean(totalPages - page);
+  const hasPreviousPage = page !== 1;
 
   return {
-    page: currentPage,
+    page,
     perPage,
-    totalItems,
+    totalItems: count,
     totalPages,
-    hasPreviousPage: currentPage > 1,
-    hasNextPage: currentPage < totalPages,
+    hasPreviousPage,
+    hasNextPage,
   };
 };
